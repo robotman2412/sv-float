7eb8c4f11e33dda39c659acc8c228cf0fb2a3951
RobotMan2412 <julian@scheffers.net>
2024-01-29 21:28:13 +0100

    Aliases

---
+++ hdl/svfloat_div.sv
@@ -0,0 +1,32 @@
+
+// Copyright © 2024, Julian Scheffers, see LICENSE for more information
+
+`timescale 1ns/1ps
+
+
+
+// Floating-point divider.
+// Shorthand for `svfloat_divdiv#(.mode("div"))`.
+module svfloat_div#(
+    // Floating-point type as described in the README.
+    type                float           = svfloat::float32,
+    // Enable pipeline register before divtiplier.
+    parameter   bit     plr_pre_div     = 0,
+    // Enable pipeline register after divtiplier.
+    parameter   bit     plr_post_div    = 0
+)(
+    // Pipeline clock.
+    // Ignored if all pipeline registers are disabled.
+    input  logic                clk,
+    
+    // Left-hand side argument.
+    input  float                lhs,
+    // Right-hand side argument.
+    input  float                rhs,
+    // Result.
+    output float                res
+);
+    svfloat_divdiv#(float, "div", plr_pre_div, plr_post_div) div(
+        clk, lhs, rhs, res
+    );
+endmodule
---
+++ hdl/svfloat_mul.sv
@@ -0,0 +1,32 @@
+
+// Copyright © 2024, Julian Scheffers, see LICENSE for more information
+
+`timescale 1ns/1ps
+
+
+
+// Floating-point multiplier.
+// Shorthand for `svfloat_muldiv#(.mode("mul"))`.
+module svfloat_mul#(
+    // Floating-point type as described in the README.
+    type                float           = svfloat::float32,
+    // Enable pipeline register before multiplier.
+    parameter   bit     plr_pre_mul     = 0,
+    // Enable pipeline register after multiplier.
+    parameter   bit     plr_post_mul    = 0
+)(
+    // Pipeline clock.
+    // Ignored if all pipeline registers are disabled.
+    input  logic                clk,
+    
+    // Left-hand side argument.
+    input  float                lhs,
+    // Right-hand side argument.
+    input  float                rhs,
+    // Result.
+    output float                res
+);
+    svfloat_muldiv#(float, "mul", plr_pre_mul, plr_post_mul) mul(
+        clk, lhs, rhs, res
+    );
+endmodule
